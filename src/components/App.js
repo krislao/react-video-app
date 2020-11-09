a8cb3429a8c380a6a91f130f234e2e812fad481a
krislao <krislao13@gmail.com>
2020-11-09 11:50:51 +0800

    Search videos by term

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,26 @@ import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import VideoDetails from "./video/Details";
 import VideoList from "./video/List";
+import youtube from "../apis/youtube";
 
 class App extends Component {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    var {
+      data: { items: videos },
+    } = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({ videos });
+  };
+
   render() {
     return (
-      <div>
-        <SearchBar />
+      <div className="ui container">
+        <SearchBar onSubmit={this.onTermSubmit} />
         <div>
           <VideoDetails />
           <VideoList />
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
-  static propTypes = {};
+  state = { term: "" };
+
+  onInputChange = (event) => {
+    this.setState({
+      term: event.target.value,
+    });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label htmlFor="search-video">Video Search:</label>
+            <input
+              type="text"
+              id="search-video"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
