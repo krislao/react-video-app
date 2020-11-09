008c1fbd3d32b021bfc90a0bb29b62c2bd4d4812
krislao <krislao13@gmail.com>
2020-11-09 14:53:28 +0800

    Render a video

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function VideoItem(props) {
-  return <li>{props.video.snippet.title}</li>;
+function VideoItem({ video }) {
+  return (
+    <li>
+      <img src={video.snippet.thumbnails.default.url} />
+      {video.snippet.title}
+    </li>
+  );
 }
 
 export default VideoItem;
